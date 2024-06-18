02a42c1f88ccf09825223cc7e4d67cefe0dbb1d4
Avijeet Prasad <prasad.avijeet@gmail.com>
2024-06-18 11:09:35 +0200

    [REP] Add .flake8 configuration and update setup.py formatting
    - Added a new .flake8 file with the following content:
      - Set max-line-length to 120 characters to allow longer lines (new file mode 100644).
    
    - Updated setup.py with the following changes for better readability and consistency:
      - Corrected spacing around the if condition and comments for Darwin system setup (lines 10-14).
      - Formatted link_opts list with consistent spacing and line breaks for readability (lines 15-18).
      - Added missing space before equal signs in variable assignments for GCC setup (lines 21-23).
      - Reformatted comp_flags lists by splitting long lines into multiple lines and adding appropriate comments (lines 30-41).
      - Fixed indentation and spacing for the Extension and setup function calls (lines 47-62).

--- setup.py
+++ setup.py
@@ -13,15 +13,16 @@ p = pathlib.Path(sys.executable)
 root_dir = str(pathlib.Path(*p.parts[0:-2]))
 
 
-if(plt.system() == 'Darwin'):
-    root_dir = '/opt/local/' # using this one if macports are installed
+if (plt.system() == 'Darwin'):
+    root_dir = '/opt/local/'  # using this one if macports are installed
     CC = 'clang'
-    CXX= 'clang++'
-    link_opts = ["-stdlib=libc++","-bundle","-undefined","dynamic_lookup", "-fopenmp","-lgomp"]
+    CXX = 'clang++'
+    link_opts = ["-stdlib=libc++", "-bundle", "-undefined",
+                 "dynamic_lookup", "-fopenmp", "-lgomp"]
 else:
-    #root_dir = '/usr/'
+    # root_dir = '/usr/'
     CC = 'gcc'
-    CXX= 'g++'
+    CXX = 'g++'
     link_opts = ["-shared", "-fopenmp"]
 
 os.environ["CC"] = CC
@@ -30,32 +31,44 @@ os.environ["CXX"] = CXX
 
 # Optimization flags. With Macs M-processor remove the -march=native!
 
-comp_flags=['-Ofast', '-flto','-g0','-fstrict-aliasing','-march=native','-mtune=native','-std=c++20','-fPIC','-fopenmp', '-I./src', "-DNPY_NO_DEPRECATED_API",'-mprefer-vector-width=256', '-DNDEBUG', '-pedantic', '-Wall']
+# comp_flags=['-Ofast', '-flto','-g0','-fstrict-aliasing','-march=native',
+# '-mtune=native','-std=c++20','-fPIC','-fopenmp', '-I./src',
+# "-DNPY_NO_DEPRECATED_API",'-mprefer-vector-width=256', '-DNDEBUG',
+#  '-pedantic', '-Wall', '-I/mn/stornext/d9/data/avijeetp/envs/pymilne/include/eigen3']
 
+comp_flags = ['-Ofast', '-g0', '-fstrict-aliasing', '-march=native',
+              '-mtune=native', '-std=c++20', '-fPIC', '-fopenmp', '-I./src',
+              "-DNPY_NO_DEPRECATED_API", '-mprefer-vector-width=256', '-DNDEBUG',
+              '-pedantic', '-Wall', '-I/mn/stornext/d9/data/avijeetp/envs/pymilne/include/eigen3']
+
+# comp_flags=['-Ofast','-g0','-fstrict-aliasing','-march=native','-mtune=native',
+# '-std=c++20','-fPIC','-fopenmp', '-I./src', "-DNPY_NO_DEPRECATED_API",'-mprefer-vector-width=256',
+# '-DNDEBUG', '-pedantic', '-Wall','-I/mn/stornext/d9/data/carlosjd/mamba_envs/kdb/include/eigen3']
 
 # Optimization flags for development
 
-#comp_flags=['-Og', '-g3','-fstrict-aliasing','-march=native','-mtune=native','-std=c++20','-fPIC','-fopenmp', '-I./src', "-DNPY_NO_DEPRECATED_API", '-pedantic', '-Wall']
+# comp_flags=['-Og', '-g3','-fstrict-aliasing','-march=native','-mtune=native','-std=c++20','-fPIC','-fopenmp',
+#  '-I./src', "-DNPY_NO_DEPRECATED_API", '-pedantic', '-Wall']
 
 
 extension = Extension("pyMilne",
-                      sources=["pyMilne.pyx", "src/wrapper_tools_spatially_coupled.cpp", "src/lm_sc.cpp", \
-                               "src/spatially_coupled_helper.cpp"], 
-                      include_dirs=["./",numpy.get_include(), './eigen3', root_dir+"/include/"],
+                      sources=["pyMilne.pyx", "src/wrapper_tools_spatially_coupled.cpp", "src/lm_sc.cpp",
+                               "src/spatially_coupled_helper.cpp"],
+                      include_dirs=[
+                          "./", numpy.get_include(), './eigen3', root_dir+"/include/"],
                       language="c++",
                       extra_compile_args=comp_flags,
                       extra_link_args=comp_flags+link_opts,
-                      library_dirs=['./',"/usr/lib/"],
+                      library_dirs=['./', "/usr/lib/"],
                       libraries=['fftw3'])
 #                      undef_macros = [ "NDEBUG" ])
 
 extension.cython_directives = {'language_level': "3"}
 
 setup(
-    name = 'pyMilne',
-    version = '3.0',
-    author = 'J. de la Cruz Rodriguez (ISP-SU 2018 - 2023)',
+    name='pyMilne',
+    version='3.0',
+    author='J. de la Cruz Rodriguez (ISP-SU 2018 - 2023)',
     ext_modules=[extension],
-    cmdclass = {'build_ext': build_ext}
+    cmdclass={'build_ext': build_ext}
 )
-
